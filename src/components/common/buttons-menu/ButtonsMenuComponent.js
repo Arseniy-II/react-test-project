5ba1519a623318aff4f9a9be72164ca649fbafe1
Arseniy-II <arseniy.public@gmail.com>
2017-12-03 13:31:28 +0700

    Add styles for Buttons

--- src/components/common/buttons-menu/ButtonsMenuComponent.js
+++ src/components/common/buttons-menu/ButtonsMenuComponent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ButtonsContainer (props) {
+export default function ButtonsMenuContainer (props) {
     return (
-        <div>
+        <div className="buttons-menu">
             {props.children}
         </div>);
 }
 
-ButtonsContainer.propTypes = {
+ButtonsMenuContainer.propTypes = {
     children: PropTypes.oneOfType([
         PropTypes.element,
         PropTypes.array
